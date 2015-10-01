14050699c3dcb4a2a2fa2001cdd248cdf564aad0
ACACIO MANFRIM COELHO <acaciocoelho@hotmail.com>
2015-09-30 21:42:58 -0300

    comit inicial

---
+++ desktop/src/br/grupointegrado/ads/flappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package br.grupointegrado.ads.flappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.grupointegrado.ads.flappybird.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 640;
+		config.width = 480;
+		new LwjglApplication(new MainGame(), config);
+	}
+}
